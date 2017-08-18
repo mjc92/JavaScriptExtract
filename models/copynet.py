6a850d2e06a2b7d5c7b485ebcf83012579751567
mjc92 <minje.choi@navercorp.com>
2017-08-18 17:38:20 +0900

    Update models

--- models/copynet.py
+++ models/copynet.py
@@ -11,21 +11,25 @@ class CopyNet(nn.Module):
     def __init__(self, args, vocab):
         super(CopyNet, self).__init__()
         
-        if args.encoder=='lstm':
-            self.encoder = nn.LSTM(input_size=args.embed, 
-                                   hidden_size=args.hidden, num_layers=1, batch_first=True,
-                                   bidirectional=True)
-        elif args.encoder=='gru':
-            self.encoder = nn.GRU(input_size=args.embed, 
-                                  hidden_size=args.hidden, num_layers=1, batch_first=True)
-        elif args.encoder=='transformer':
-            from models.transformer.Models import Encoder
-            self.encoder = Encoder(
-                n_max_seq=args.max_in_seq, n_layers=args.n_layers, n_head=args.n_head,
-                d_word_vec=args.embed, d_model=args.hidden,
-                d_inner_hid=args.hidden*2, dropout=0.1)
-        
+        # if args.encoder=='lstm':
+        #     self.encoder = nn.LSTM(input_size=args.embed, 
+        #                            hidden_size=args.hidden, num_layers=1, batch_first=True,
+        #                            bidirectional=True)
+        # elif args.encoder=='gru':
+        #     self.encoder = nn.GRU(input_size=args.embed, 
+        #                           hidden_size=args.hidden, num_layers=1, batch_first=True)
+        # elif args.encoder=='transformer':
+        #     from models.transformer.Models import Encoder
+        #     self.encoder = Encoder(
+        #         n_max_seq=args.max_in_seq, n_layers=args.n_layers, n_head=args.n_head,
+        #         d_word_vec=args.embed, d_model=args.hidden,
+        #         d_inner_hid=args.hidden*2, dropout=0.1)
         self.transformer = True if args.encoder=='transformer' else False
+
+        self.encoder = nn.GRU(input_size=args.embed, 
+                              hidden_size=args.hidden, num_layers=1, batch_first=True,
+                              bidirectional=True)
+        
         self.embedding = nn.Embedding(vocab.count, args.embed)
         
         self.decoder = CopyDecoder(args, vocab, self.embedding)
